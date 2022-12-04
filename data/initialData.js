a1baf83802b321b71e22be237456733913495dd2
MikhailEzhov <Hammer.perm@gmail.com>
2022-12-04 21:01:02 +0500

    feat: added initial data

---
+++ data/initialData.js
@@ -0,0 +1,16 @@
+const initialData = [
+  { id: 1, eng: "A bit", rus: "Немного" },
+  {
+    id: 2,
+    eng: "A drop in the bucket",
+    rus: "Капля в море",
+  },
+  { id: 3, eng: "A few", rus: "Несколько" },
+  {
+    id: 4,
+    eng: "Can you show it on the map, please?",
+    rus: "Можете показать это на карте, пожалуйста?",
+  },
+];
+
+export default initialData;
